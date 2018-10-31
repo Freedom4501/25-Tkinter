18259bf9ef20aa3d87f47c5012fa2a68c6e0077e
lim5 <lim5@rose-hulman.edu>
2018-10-31 16:38:21 -0400

    111

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -2,8 +2,8 @@
 This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Mashengjun Li.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -15,23 +15,39 @@ def main():
     # TODO: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
+    root = tkinter.Tk()
 
     # ------------------------------------------------------------------
     # TODO: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
-
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
     # ------------------------------------------------------------------
     # TODO: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
-
+    button = ttk.Button(frame1, text= 'LMAO')
+    button.grid()
     # ------------------------------------------------------------------
     # TODO: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
-
+    print_stuff_button = ttk.Button(frame1, text='Print Stuff')
+    print_stuff_button['command']= (lambda:
+                                    own_function())
+    print_stuff_button.grid()
+    entry = ttk.Entry(frame1)
+    entry.grid()
+    button2 = ttk.Button(frame1, text='ENTER')
+    print_stuff_button['command'] = (lambda:
+                                     n)
+    button2.grid()
+    root.mainloop()
+def n(entry):
+    contents_of_entry_box = entry.get()
+    print(contents_of_entry_box)
     # ------------------------------------------------------------------
     # TODO: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
@@ -40,6 +56,8 @@ def main():
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+def own_function():
+    print('Hello')
 
     # ------------------------------------------------------------------
     # TODO: 7.
